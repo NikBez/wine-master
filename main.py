a5f175d0ba5618509c506f60a7232e604e4bda43
Kit <nikitabit@yandex.ru>
2023-01-12 20:34:42 +0700

    Made corrections after the review

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-
 from utils import get_year_word, get_struct_data_from_exel
 
-items_structure = get_struct_data_from_exel()
+YEAR_OF_FOUNDATION = 1920
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-template = env.get_template('template.html')
+def main():
+    catalog = get_struct_data_from_exel()
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
 
-year = datetime.datetime.now().year - 1920
-year_word = get_year_word(str(year))
+    year = datetime.datetime.now().year - YEAR_OF_FOUNDATION
+    year_word = get_year_word(str(year))
 
-context = {
-    "year": year,
-    "year_word": year_word,
-    "items_structure": items_structure,
-}
-rendered_page = template.render(**context)
+    context = {
+        "year": year,
+        "year_word": year_word,
+        "catalog": catalog,
+    }
+    rendered_page = template.render(**context)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,10 +1,13 @@
+import argparse
+import sys
 from collections import defaultdict
 
 from pandas import read_excel
 
 
-# Функция возвращает правильное слово в зависимости от года, который в нее передается
 def get_year_word(year):
+    """ Функция возвращает правильное слово в зависимости от года, который в нее передается. """
+
     if year.endswith(('11', '12', '13', '14')):
         return 'лет'
     elif year.endswith('1'):
@@ -14,23 +17,34 @@ def get_year_word(year):
     else:
         return 'лет'
 
-# Функция считывает данные из exel таблицы и возвращает вложенную структуру
-def get_struct_data_from_exel():
-    items_structure = defaultdict(list)
 
-    items_from_exel = read_excel(io='wines.xlsx',
-                                 sheet_name='wines',
-                                 usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'],
-                                 na_values='Сорт неизвестен',
-                                 keep_default_na=False,
-                                 )
-    wines = items_from_exel.to_dict(orient="records")
+def get_struct_data_from_exel():
+    """ Функция считывает данные из exel таблицы и возвращает вложенную структуру. """
+
+    catalog = defaultdict(list)
+
+    parser = argparse.ArgumentParser(description="Скрипт создает страницу сайта и подгружает в нее ассортимент из файла wines.xlsx")
+    parser.add_argument('-p', '--path', default='wines.xlsx', help="Путь к файлу с каталогом")
+    args = parser.parse_args()
+
+    try:
+        from_exel = read_excel(io=f'{args.path}',
+                               sheet_name='wines',
+                               usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'],
+                               na_values='Сорт неизвестен',
+                               keep_default_na=False,
+                               )
+    except IOError:
+        print('File not found')
+        sys.exit()
+
+    wines = from_exel.to_dict(orient="records")
 
     for wine in wines:
-        items_structure[wine['Категория']].append({'Название': wine['Название'],
-                                                   'Сорт': wine['Сорт'],
-                                                   'Цена': wine['Цена'],
-                                                   'Картинка': wine['Картинка'],
-                                                   'Акция': wine['Акция']
-                                                   })
-    return items_structure
+        catalog[wine['Категория']].append({'Название': wine['Название'],
+                                           'Сорт': wine['Сорт'],
+                                           'Цена': wine['Цена'],
+                                           'Картинка': wine['Картинка'],
+                                           'Акция': wine['Акция']
+                                           })
+    return catalog
